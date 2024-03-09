fa2a7b0aae00043cd4a02f8b38b15566aec3ea55
Jonas Myrenås <jonas.myrenas@mindler.se>
2024-03-09 18:57:16 +0100

    bump

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -15,7 +15,7 @@ import { registerRoute } from "workbox-routing";
 import { NetworkFirst } from "workbox-strategies";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const appVersion = "0.1.5";
+const appVersion = "0.1.6";
 
 declare const self: ServiceWorkerGlobalScope;
 
